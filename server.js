f4e9f92be3865a36f36b956b452b64b52401773e
RijenMaharjan <rijenmhrzn94@gmail.com>
2025-02-27 11:21:21 +0545

    environment established

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 //Imports
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
 import express from "express";
+import dotenv from "dotenv";
+const colors = require("colors");
+//DOT ENV config
+dotenv.config();
 
 //rest object
 const app = express();
@@ -9,7 +15,13 @@ app.get("/", (req, res) => {
   res.send("<h1>Welcome to JOB PORTAL</h1>");
 });
 
+//port
+const PORT = process.env.PORT || 8080;
+
 //listen
-app.listen(8080, () => {
-  console.log("Node Server Running");
+app.listen(PORT, () => {
+  console.log(
+    `Node Server Running In ${process.env.DEV_MODE} Mode on port no. ${PORT}`
+      .bgCyan.white
+  );
 });
