8f40986445fbaad51a4b27d2ba45cc4b7f6ac535
RijenMaharjan <rijenmhrzn94@gmail.com>
2025-02-27 14:06:16 +0545

    application setup completed

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-//Imports
+//Packages Imports
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 const colors = require("colors");
 import express from "express";
 import dotenv from "dotenv";
+//files imports
+import cors from "cors";
+import morgan from "morgan";
 import { connect } from "http2";
 import connectDB from "./config/db.js";
 import testRoutes from "./routes/testRoutes.js";
@@ -19,6 +22,8 @@ const app = express();
 
 //middlewares
 app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
 
 //route
 app.use("/api/v1/test", testRoutes);
