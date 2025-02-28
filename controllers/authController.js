e96c87ccc068c74fd65ec6c0fd3df1d2c43995fd
RijenMaharjan <rijenmhrzn94@gmail.com>
2025-02-28 10:52:42 +0545

    simplified err handling and err handling for duplicate key done

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,20 +2,6 @@ import userModels from "../models/userModels.js";
 
 export const registerController = async (req, res, next) => {
   const { name, email, password } = req.body;
-  //validate
-  if (!name) {
-    next("Name is required");
-  }
-  if (!email) {
-    next("please provide email");
-  }
-  if (!password) {
-    next("please provide password and atleast 6 characters");
-  }
-  const existingUser = await userModels.findOne({ email });
-  if (existingUser) {
-    next("Email already registered. Please login");
-  }
   const user = await userModels.create({ name, email, password });
   res.status(201).send({
     success: true,
--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -12,6 +12,13 @@ const errorMiddleware = (err, req, res, next) => {
         .map((item) => item.message)
         .join(","));
   }
+  //duplicate error
+  if (err.code && err.code === 11000) {
+    (defaultErrors.statusCode = 400),
+      (defaultErrors.message = `${Object.keys(
+        err.keyValue
+      )} field has to be unique`);
+  }
 
   res.status(defaultErrors.statusCode).json({ message: defaultErrors.message });
 };
