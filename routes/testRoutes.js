5ea92bfafde10238a48789338692879386bb5c5e
RijenMaharjan <rijenmhrzn94@gmail.com>
2025-02-28 12:24:53 +0545

    two step authentication

--- routes/testRoutes.js
+++ routes/testRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { testPostController } from "../controllers/testController.js";
+import userAuth from "../middlewares/authMiddleware.js";
 
 //route object
 const router = express.Router();
 
-//routes
-router.post("/test-post", testPostController);
+//routes middleware in middle
+router.post("/test-post", userAuth, testPostController);
 
 //export
 export default router;
