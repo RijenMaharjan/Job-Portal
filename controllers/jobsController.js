13ecfcebf62361e6b0091987cb0decb97b9dc804
RijenMaharjan <rijenmhrzn94@gmail.com>
2025-02-28 16:15:55 +0545

    job creation

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,1 +1,15 @@
-export const createJobController = async () => {};
+import jobModels from "../models/jobModels.js";
+
+//Create job
+export const createJobController = async (req, res, next) => {
+  const { company, position } = req.body;
+  if (!company || !position) {
+    next("Please provide all fields.");
+  }
+  req.body.createdBy = req.user.userId;
+  const job = await jobModels.create(req.body);
+  res.status(201).json({ job });
+};
+
+//get job
+export const getAllJobsController = () => {};
--- routes/jobsRoutes.js
+++ routes/jobsRoutes.js
@@ -1,11 +1,17 @@
 import express from "express";
 import userAuth from "../middlewares/authMiddleware.js";
-import { createJobController } from "../controllers/jobsController.js";
+import {
+  createJobController,
+  getAllJobsController,
+} from "../controllers/jobsController.js";
 
 const router = express.Router();
 
 //routes
 // CREATE JOB || POST
 router.post("/create-job", userAuth, createJobController);
+
+// GET JOB || GET
+router.post("/get-jobs", userAuth, getAllJobsController);
 
 export default router;
